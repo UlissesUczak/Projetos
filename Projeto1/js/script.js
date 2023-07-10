63d014c23a705a41ac3f4da7d9ce0c612d648267
Ulisses U <uczakulisses@gmail.com>
2023-07-10 10:10:26 -0300

    exercicios de pratica

---
+++ Projeto1/js/script.js
@@ -0,0 +1,8 @@
+// Elementos
+
+// campo de notas
+const notesContainer = document.querySelector("#notes-container")
+// campo te texto
+const noteInput = document.querySelector("#note-content")
+// botão de adicionar
+const addNoteBtn = document.querySelector(".add-note")
